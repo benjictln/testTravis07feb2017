87fdeddfc3f5f477b1be28976d4cbe037660fe05
Benjamin Castellan <benjamincastellan@gmail.com>
2017-02-18 13:26:47 +0100

    add android project

---
+++ testIntent/app/src/main/java/com/example/benja/testintent/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.benja.testintent;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import static android.net.Uri.parse;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button b1 = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 = (Button) findViewById(R.id.button);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri u1 = parse("tel:0752633548");
+                Intent i1 = new Intent(Intent.ACTION_DIAL);
+                i1.setData(u1);
+                if (PackageManager.PERMISSION_GRANTED != ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE)) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(i1);
+            }
+        });
+    }
+}
+
